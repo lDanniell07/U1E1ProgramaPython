d4d2be55e1398a85cdc6e14a94689abef0afd89b
lDanniell07 <56766515+lDanniell07@users.noreply.github.com>
2020-09-30 10:07:52 -0500

    Update programa.py

--- python/programa.py
+++ python/programa.py
@@ -7,9 +7,17 @@ def Ordena(array2,array1):
                 array1[j+1] = temporal
     return array1
 print("------------------------")
-fra = 'Me llamo daniel'
-frase = fra.split(' ')
-archivo = open("frase.txt")  
-texto = archivo.read().split(' ')
+frase = ["Me","llamo","daniel"]
+
+archivo2 = open("demo.txt")  
+texto = archivo2.read().split(' ')
+
+for i in range(len(frase)-1):
+        for j in range(len(frase)-1):
+            if not(texto[j] == frase[j]):
+                temporal = texto[j]
+                texto[j] = texto[j+1]
+                texto[j+1] = temporal
+
 print("Frase Ordenada:")
-print(Ordena(frase,texto))
+print(texto)
